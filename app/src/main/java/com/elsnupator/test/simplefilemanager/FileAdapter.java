cb4ab8ad9f23873403c98d0a1ec5978e267b2128
Lukas Ruzicka <lukas.ruzicka96@seznam.cz>
2018-02-22 21:36:05 +0100

    Navigation through folders works

--- app/src/main/java/com/elsnupator/test/simplefilemanager/FileAdapter.java
+++ app/src/main/java/com/elsnupator/test/simplefilemanager/FileAdapter.java
@@ -1,6 +1,5 @@
 package com.elsnupator.test.simplefilemanager;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
@@ -14,28 +13,37 @@ import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class FileAdapter extends BaseAdapter {
 
-    private List<File> folders;
-    private List<File> files;
-    private Activity activity;
+    private File currentFolder;
+    private List<File> folders = new ArrayList<>();
+    private List<File> files = new ArrayList<>();
+    private MainActivity activity;
 
-    FileAdapter(File currentFolder, Activity activity){
-        this.activity = activity;
-        folders = new ArrayList<>(Arrays.asList(currentFolder.listFiles(new FileFilter() {
+    FileAdapter(File currentFolder, MainActivity mainActivity){
+        this.currentFolder = currentFolder;
+        this.activity = mainActivity;
+        File[] foldersArray = currentFolder.listFiles(new FileFilter() {
             @Override
             public boolean accept(File file) {
-                return file.isDirectory();
+                return file.isDirectory() && !file.getName().startsWith(".");
             }
-        })));
-        files = new ArrayList<>(Arrays.asList(currentFolder.listFiles(new FileFilter() {
+        });
+        if(foldersArray != null)
+            folders = new ArrayList<>(Arrays.asList(foldersArray));
+        File[] filesArray = currentFolder.listFiles(new FileFilter() {
             @Override
             public boolean accept(File file) {
-                return !file.isDirectory();
+                return !file.isDirectory() && !file.getName().startsWith(".");
             }
-        })));
+        });
+        if(filesArray != null)
+            files = new ArrayList<>(Arrays.asList(filesArray));
+        Collections.sort(folders);
+        Collections.sort(files);
     }
 
     @Override
@@ -59,7 +67,7 @@ public class FileAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) this.activity
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -72,10 +80,24 @@ public class FileAdapter extends BaseAdapter {
         if(position == 0){
             icon.setImageDrawable(ContextCompat.getDrawable(activity,R.drawable.icon_up));
             name.setText(activity.getResources().getString(R.string.up));
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!currentFolder.getName().equals(""))
+                        activity.setCurrentFolder(currentFolder.getParentFile());
+                }
+            });
         }
         else if(position <= folders.size()){
             icon.setImageDrawable(ContextCompat.getDrawable(activity,R.drawable.icon_folder));
             name.setText(folders.get(position-1).getName());
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    activity.setCurrentFolder(new File(currentFolder.getAbsolutePath() + File.separator +
+                            folders.get(position-1).getName()));
+                }
+            });
         }
         else{
             icon.setImageDrawable(ContextCompat.getDrawable(activity,R.drawable.icon_file));
--- app/src/main/java/com/elsnupator/test/simplefilemanager/MainActivity.java
+++ app/src/main/java/com/elsnupator/test/simplefilemanager/MainActivity.java
@@ -1,8 +1,15 @@
 package com.elsnupator.test.simplefilemanager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.*;
@@ -13,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AbsListView filesView;
     private File currentFolder;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +32,29 @@ public class MainActivity extends AppCompatActivity {
 
         filesView = findViewById(R.id.files_view);
 
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            }
+        }
 
-        currentFolder = getObbDir().getParentFile();
+        currentFolder = new File("/");
     }
 
     @Override
     protected void onResume(){
         super.onResume();
         refreshFiles();
+    }
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            Toast.makeText(this, "No", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show();
     }
 
 
@@ -42,8 +65,15 @@ public class MainActivity extends AppCompatActivity {
                 filesView.setAdapter(new FileAdapter(currentFolder,MainActivity.this));
             }
         }.run();
+        Log.i(TAG,"Files refreshed");
     }
 
+
+    void setCurrentFolder(File folder){
+        this.currentFolder = folder;
+        Log.i(TAG,"Current folder: " + folder.getAbsolutePath());
+        refreshFiles();
+    }
 
 
     // Overflow menu
